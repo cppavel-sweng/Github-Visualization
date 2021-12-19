815a8c4107d593fc991c96959443eb96ae850acf
cppavel <petrukhinpavel19@gmail.com>
2021-12-19 18:40:46 +0000

    check that the setup allows storing and retrieving

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import pymongo
 from pymongo import MongoClient
 
 app = Flask(__name__)
+
+def drop():
+    client = MongoClient(host='test_mongodb',
+                            port=27017, 
+                            username='root', 
+                            password='pass',
+                            authSource="admin")
+
+    mydb = client["animal_db"]
+    collection = mydb["animal_tb"]
+    collection.drop()
+
+def put_new_animal(animal_name, animal_type):
+    client = MongoClient(host='test_mongodb',
+                            port=27017, 
+                            username='root', 
+                            password='pass',
+                            authSource="admin")
+
+    mydb = client["animal_db"]
+    collection = mydb["animal_tb"]
+    dict = {"id": 100, "name": animal_name, "type": animal_type}
+    x = collection.insert_one(dict)
+
+    return x
+
 
 def get_db():
     client = MongoClient(host='test_mongodb',
@@ -23,6 +49,28 @@ def get_stored_animals():
     _animals = db.animal_tb.find()
     animals = [{"id": animal["id"], "name": animal["name"], "type": animal["type"]} for animal in _animals]
     return jsonify({"animals": animals})
+
+
+@app.route('/drop')
+def get_drop():
+  drop()
+
+@app.route('/form-example', methods=['GET', 'POST'])
+def form_example():
+    if request.method == 'POST':
+        animal_name = request.form.get('animal_name')
+        animal_type = request.form.get('animal_type')
+        result = put_new_animal(animal_name, animal_type)
+
+
+        return f'''Successfully added to database {animal_name}, {animal_type}: {result} </h1>'''
+                  
+    return '''
+           <form method="POST">
+               <div><label>Animal Name: <input type="text" name="animal_name"></label></div>
+               <div><label>Type: <input type="text" name="animal_type"></label></div>
+               <input type="submit" value="Submit">
+           </form>'''
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
